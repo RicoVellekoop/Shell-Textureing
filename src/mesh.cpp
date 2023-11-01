97089f6f9ed5fda2cca3cf697ea27425ec3ee617
RicoVellekoop <ricovellekoop@gmail.com>
2023-11-01 23:49:16 +0100

    Add more meshes

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,6 +1,6 @@
 #include "mesh.h"
 
-Mesh::Mesh(const std::vector<Vertex> &vertices) : vertexCount(vertices.size())
+Mesh::Mesh(const std::vector<Vertex> &vertices, const std::vector<unsigned int> &indices) : vertexCount(vertices.size()), indexCount(indices.size())
 {
     // Generate and bind the Vertex Array Object (VAO)
     glGenVertexArrays(1, &VAO);
@@ -9,6 +9,10 @@ Mesh::Mesh(const std::vector<Vertex> &vertices) : vertexCount(vertices.size())
     // Generate and bind the Vertex Buffer Object (VBO)
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // Generate and bind the Element Buffer Object (EBO)
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
     // Copy the vertex data to the buffer
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
@@ -20,6 +24,9 @@ Mesh::Mesh(const std::vector<Vertex> &vertices) : vertexCount(vertices.size())
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, normal));
     glEnableVertexAttribArray(2);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, uv));
+
+    // Copy the index data to the buffer
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 
     // Unbind the VAO and VBO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -37,10 +44,10 @@ void Mesh::Render()
 {
     // Bind the VAO
     glBindVertexArray(VAO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
     // Draw the mesh
-    glDrawArrays(GL_TRIANGLES, 0, vertexCount);
-
+    glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
     // Unbind the VAO
     glBindVertexArray(0);
 }
@@ -53,6 +60,101 @@ Mesh *Mesh::CreateTriangle()
         {{0.5f, -0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.75f, 0.0f}},
         {{0.0f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.5f, 1.0f}}};
 
+    // Create the indices
+    std::vector<unsigned int> indices = {0, 1, 2};
+
     // Create the mesh
-    return new Mesh(vertices);
+    return new Mesh(vertices, indices);
+}
+
+Mesh *Mesh::CreateQuad()
+{
+    std::vector<Vertex> vertices = {
+        {{-1.0f, -1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f}},
+        {{1.0f, -1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 0.0f}},
+        {{-1.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 2.0f}},
+        {{1.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}}};
+
+    std::vector<unsigned int> indices = {0, 1, 2, 2, 1, 3};
+
+    return new Mesh(vertices, indices);
+}
+
+Mesh *Mesh::CreateCube()
+{
+    std::vector<Vertex> vertices = {
+        // Front
+        {{-1.0f, -1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f}},
+        {{1.0f, -1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 0.0f}},
+        {{-1.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
+        {{1.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
+        // Back
+        {{1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, 0.0f}},
+        {{-1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f}},
+        {{1.0f, 1.0f, -1.0f}, {0.0f, 0.0f, -1.0f}, {0.0f, 1.0f}},
+        {{-1.0f, 1.0f, -1.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 1.0f}},
+        // Left
+        {{-1.0f, -1.0f, -1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+        {{-1.0f, -1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+        {{-1.0f, 1.0f, -1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+        {{-1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+    };
+
+    std::vector<unsigned int>
+        indices = {// Front
+                   0, 1, 2, 2, 1, 3,
+                   // Back
+                   4, 5, 6, 6, 5, 7,
+                   // Left
+                   8, 9, 10, 10, 9, 11,
+                   // Right
+                   12, 13, 14, 14, 13, 15,
+                   // Top
+                   16, 17, 18, 18, 17, 19,
+                   // Bottom
+                   20, 21, 22, 22, 21, 23};
+
+    return new Mesh(vertices, indices);
+}
+
+Mesh *Mesh::CreateSphere(int stacks, int slices)
+{
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+
+    // Create the vertices
+    for (int i = 0; i <= stacks; i++)
+    {
+        float V = i / (float)stacks;
+        float phi = V * glm::pi<float>();
+
+        // Loop Through Slices
+        for (int j = 0; j <= slices; j++)
+        {
+            float U = j / (float)slices;
+            float theta = U * (glm::pi<float>() * 2);
+
+            // Calc The Vertex Positions
+            float x = cos(theta) * sin(phi);
+            float y = cos(phi);
+            float z = sin(theta) * sin(phi);
+
+            // Push Back Vertex Data
+            vertices.push_back({{x, y, z}, {x, y, z}, {U, V}});
+        }
+    }
+
+    // Calc The Index Positions
+    for (int i = 0; i < slices * stacks + slices; i++)
+    {
+        indices.push_back(i);
+        indices.push_back(i + slices + 1);
+        indices.push_back(i + slices);
+
+        indices.push_back(i + slices + 1);
+        indices.push_back(i);
+        indices.push_back(i + 1);
+    }
+
+    return new Mesh(vertices, indices);
 }
--- src/mesh.h
+++ src/mesh.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <glm/vec3.hpp>
 #include <glm/vec2.hpp>
+#include <glm/gtc/constants.hpp>
 #include <GL/glew.h>
 #include <cmath>
 
@@ -17,14 +18,17 @@ struct Vertex
 class Mesh
 {
 public:
-    Mesh(const std::vector<Vertex> &vertices);
+    Mesh(const std::vector<Vertex> &vertices, const std::vector<unsigned int> &indices);
     ~Mesh();
     void Render();
     static Mesh *CreateTriangle();
+    static Mesh *CreateQuad();
+    static Mesh *CreateCube();
+    static Mesh *CreateSphere(int stacks, int slices);
 
 private:
-    GLuint VAO, VBO;
-    GLsizei vertexCount;
+    GLuint VAO, VBO, EBO;
+    GLsizei vertexCount, indexCount;
 };
 
 #endif
